22eed681aafd6f9fd2c12101fd026f1b650dd936
PascalVeraguth <pascal.veraguth@cyrotech.ch>
2025-05-31 16:46:54 +0200

    change index.ts to old version i guess

--- src/index.ts
+++ src/index.ts
@@ -2,9 +2,7 @@
 console.log('Hello world!');
 
 const thisIsAnUnsusedVariable = 'Hello world!';
-console.log(thisIsAnUnsusedVariable);
 
 const thisIsAnUnusedFunction = () => {
   console.log('Hello world!');
 };
-thisIsAnUnusedFunction();
