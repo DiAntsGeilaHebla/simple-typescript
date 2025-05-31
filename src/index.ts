4117aa24041d56029c786d6190c5784fd0154991
PascalVeraguth <pascal.veraguth@cyrotech.ch>
2025-05-31 16:38:40 +0200

    fix typescript

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,10 @@
 // eslint-disable-next-line no-console
+console.log('Hello world!');
+
+const thisIsAnUnsusedVariable = 'Hello world!';
+console.log(thisIsAnUnsusedVariable);
+
+const thisIsAnUnusedFunction = () => {
   console.log('Hello world!');
-
-  const thisIsAnUnsusedVariable = 'Hello world!';
-
-    const thisIsAnUnusedFunction = () => {
-    console.log('Hello world!');
-    };
+};
+thisIsAnUnusedFunction();
