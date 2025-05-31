9352f52c40341e7863c1383460081a999a1982f7
PascalVeraguth <pascal.veraguth@cyrotech.ch>
2025-05-31 18:02:48 +0200

    Update index

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,7 @@
-// eslint-disable-next-line no-console
 console.log('Hello world!');
 
-const thisIsAnUnsusedVariable = 'Hello world!';
-console.log(thisIsAnUnsusedVariable);
+const thisIsAnUnusedVariable = 'Hello world!';
+console.log(thisIsAnUnusedVariable);
 
 const thisIsAnUnusedFunction = () => {
   console.log('Hello from the unused function!');
